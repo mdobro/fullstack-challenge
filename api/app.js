a82fc8e21f9eb041526a1340d8fe109cc4ef6eca
Mike Dobrowolski <mdobro@umich.edu>
2019-03-18 03:22:08 -0400

    adds get and create endpoints, saving to db

--- api/app.js
+++ api/app.js
@@ -1,10 +1,10 @@
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
 
 app.use(express.json())
 
 //add routers
-var contacts = require('./contacts')
+let contacts = require('./router/contacts')
 app.use('/contacts', contacts)
 
 console.log("Starting server on port 3000")
