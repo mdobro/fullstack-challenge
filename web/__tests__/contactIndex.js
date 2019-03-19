7ca837ad87b41b2de53b2d94b302f4ec555a1a47
Mike Dobrowolski <mdobro@umich.edu>
2019-03-19 13:31:08 -0400

    removes commented code

--- web/__tests__/contactIndex.js
+++ web/__tests__/contactIndex.js
@@ -12,16 +12,4 @@ test('snapshot of contactsScreen', (done) => {
     expect(tree).toMatchSnapshot();
     done()
   })
-
-  // // manually trigger the callback
-  // tree.props.onMouseEnter();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
-
-  // // manually trigger the callback
-  // tree.props.onMouseLeave();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
 });
