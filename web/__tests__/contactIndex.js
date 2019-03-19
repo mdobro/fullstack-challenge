d23088679cb8ed5f3a51c259809bed00bbcc387a
Mike Dobrowolski <mdobro@umich.edu>
2019-03-19 13:30:39 -0400

    update test name

--- web/__tests__/contactIndex.js
+++ web/__tests__/contactIndex.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import api from '../api/api'
 jest.mock('../api/api')
 
-test('Link changes the class when hovered', (done) => {
+test('snapshot of contactsScreen', (done) => {
   api.getContacts()
   .then(res => {
     const component = renderer.create(<ContactsScreen contacts={res}/>,);
